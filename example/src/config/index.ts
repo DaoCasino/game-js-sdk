cbc7f32ffcc082afafa9fa3f7a4865ef9abe6070
Sergey Govyazin <SGV123@yandex.ru>
2020-05-29 16:45:44 +0300

    [DPM-76] Redirect URL changed to origin

--- example/src/config/index.ts
+++ example/src/config/index.ts
@@ -2,5 +2,5 @@ export default {
     backendAddr: "localhost:8080",
     casinoName: "ttcasino",
     walletUrl: "https://app.dev.daowallet.com/",
-    walletAuthRedirectUrl: "http://localhost:3000"
+    walletAuthRedirectUrl: document.location.origin
 }
